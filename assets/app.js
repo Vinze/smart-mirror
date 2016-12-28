d3825e27a2de002c277f4b404350bb6233ef0c2a
Vincent Bremer <vbremer89@gmail.com>
2016-12-29 00:53:01 +0100

    Fullscreen

--- assets/app.js
+++ assets/app.js
@@ -4,19 +4,22 @@ new Vue({
         time: '',
         date: ''
     },
+    methods: {
+        fullscreen: function() {
+            screenfull.toggle();
+        }
+    },
     created: function() {
         this.time = moment().format('HH:mm');
-        this.date = moment().format('dddd D MMMM YYYY');
+        this.date = moment().format('dd D MMM YYYY');
 
         setInterval(function() {
             this.time = moment().format('HH:mm');
-            this.date = moment().format('dddd D MMMM YYYY');
+            this.date = moment().format('dd D MMM YYYY');
         }.bind(this), 5000);
     }
 });
 
-axios.get('http://www.nu.nl/rss').then(function(data) {
-    console.log(data);
-});
-
-console.log(mainWindow)
+// axios.get('http://www.nu.nl/rss').then(function(data) {
+//     console.log(data);
+// });
